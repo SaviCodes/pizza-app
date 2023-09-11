3c6ad8810d24417f8593289950c9c9307827643d
saviour <saviourtari@gmail.com>
2023-09-11 16:39:25 +0100

    first commit

---
+++ src/List/MenuList.jsx
@@ -0,0 +1,39 @@
+import Pepperoni from '../images/Pepperoni.jpg'
+import Margherita from '../images/Margherita.jpg'
+import Savvyspec from '../images/Savvyspec.jpg'
+import Vegan from '../images/Vegan.jpg'
+import Pineapple from '../images/Pineapple.jpg'
+import Expensive from '../images/Expensive.jpg'
+
+export const MenuList = [
+    {
+        name: 'Pepperoni pizza',
+        image: Pepperoni,
+        price: 15.99,
+    },
+    {
+        name: 'Margherita Pizza',
+        image: Margherita,
+        price: 23.55,
+    },
+    {
+        name: 'Savvyspec pizza',
+        image: Savvyspec,
+        price: 18.55,
+    },
+    {
+        name: 'Vegan pizza',
+        image: Vegan,
+        price: 25.15,
+    },
+    {
+        name: 'Pineapple pizza',
+        image: Pineapple,
+        price: 25.15,
+    },
+    {
+        name: 'Expensive pizza',
+        image: Expensive,
+        price: 40.99,
+    }
+]
---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Multiple from '../images/Multiple.jpg'
+import '../styles/About.css'
+
+const About = () => {
+  return (
+    <div className='about'>
+      <div className='aboutTop' style={{ backgroundImage: `url(${Multiple})` }}>
+
+      </div>
+      <div className='aboutBottom'>
+        <h1> ABOUT US </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius laboriosam maxime soluta cum voluptas tempore, culpa aliquid magni officiis eligendi blanditiis, voluptatum distinctio optio. Reprehenderit incidunt pariatur culpa assumenda in amet aut quisquam quibusdam provident, id eum, vel quia asperiores praesentium dolor ab doloremque minima exercitationem perspiciatis delectus aliquam consequatur nisi sit voluptas! Voluptatem culpa reprehenderit quasi. Explicabo temporibus, nobis, illum ad consectetur repudiandae eveniet vero deserunt quos error corrupti voluptas velit voluptate! Possimus dolorum atque, dignissimos ex voluptates debitis mollitia minus est harum eveniet quos doloribus, eaque molestiae repudiandae quam earum amet iste architecto. Harum officiis distinctio iusto a earum, quisquam molestias ratione asperiores consectetur ut cum officia ipsum mollitia necessitatibus autem .</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
